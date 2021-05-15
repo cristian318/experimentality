c425e9da573ccf22b9c9392a4fbdce1b9ecedbcc
Cristian Esteban Garzon Lopez <cristian.garzon@masivian.com>
2021-05-15 17:34:01 -0500

    Se maqueta el componente de productos mas buscados

--- src/app/components/most-wanted-products/most-wanted-products.component.ts
+++ src/app/components/most-wanted-products/most-wanted-products.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+
+// import Swiper core and required modules
+import SwiperCore, { Pagination } from 'swiper/core';
+
+// install Swiper modules
+SwiperCore.use([Pagination]);
 
 @Component({
   selector: 'app-most-wanted-products',
   templateUrl: './most-wanted-products.component.html',
-  styleUrls: ['./most-wanted-products.component.scss']
+  styleUrls: ['./most-wanted-products.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class MostWantedProductsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
