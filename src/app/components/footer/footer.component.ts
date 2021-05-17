0a48f3afcced2b20f2ea8dbfecf4556e5474560d
Cristian Esteban Garzon Lopez <cristian.garzon@masivian.com>
2021-05-17 16:57:16 -0500

    Se ajusta el footer para compatibilidad con moviles

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toggleShow(event: any) {
+    const parent = event.target.parentElement;
+    if (!parent.classList.contains('footer__top-item--show')) {
+      parent.classList.add('footer__top-item--show');
+      parent.classList.remove('footer__top-item--hide');
+    } else {
+      parent.classList.add('footer__top-item--hide');
+      parent.classList.remove('footer__top-item--show');
+    }
   }
-
 }
